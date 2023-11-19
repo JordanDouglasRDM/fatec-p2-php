12fc3e8b9514118ab083f19edc169afcd87c3094
Jordan Douglas <jordandouglas8515@gmail.com>
2023-11-19 20:39:45 -0300

    refactor: separado style da navbar.

--- navbar.php
+++ navbar.php
@@ -4,41 +4,7 @@ if (isset($_SESSION['user_id'])) {
     require_once 'repository.php';
     $dataUser = getUserById($_SESSION['user_id']);
 ?>
-<style>
-    .my-name {
-        margin-right: 10%;
-        margin-bottom: 3px;
-        color: #DDF2FD;
-    }
-    .nav {
-        background-color: #164863;
-        color: #DDF2FD;
-    }
-    .nav-title {
-        color: #DDF2FD;
-    }
-    .nav-title:hover {
-        color: orange;
-    }
-    .item-link {
-        color: #DDF2FD;
-        margin-right: 5vw;
-    }
-    .item-link:hover {
-        color: orange;
-    }
-
-    .home {
-        margin-left: -5vw;
-    }
-    .button-exit {
-        margin-right: 3vw;
-        padding: 0.5vh 2vw;
-        background-color: red;
-        color: white;
-    }
-    
-</style>
+    <link rel="stylesheet" href="css/style-navbar.css">
     <nav class="navbar navbar-expand-lg <!--navbar-dark bg-dark--> nav">
         <a class="navbar-brand nav-title" href="home.php">Gerenciador de Listas</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
